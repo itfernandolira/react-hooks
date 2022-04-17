8c66a7bc699d599ef275b84027f433b778dbfaea
Fernando Lira <it.fernandolira@gmail.com>
2022-04-17 10:32:18 +0100

    useContext - part 2

--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -3,6 +3,7 @@ import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 
 import DataContext from '../../data/DataContext'
+import { AppContext } from '../../data/Store'
 
 const UseContext = (props) => {
 
@@ -14,6 +15,8 @@ const UseContext = (props) => {
             number: state.number + delta,
         })
     }
+
+    const { number, text, setNumber } = useContext(AppContext)
 
     return (
         <div className="UseContext">
@@ -31,6 +34,24 @@ const UseContext = (props) => {
                         -1
                     </button>
                     <button className="btn" onClick={() => addNumber(1)}>
+                        +1
+                    </button>
+                </div>
+            </div>
+
+            <SectionTitle title="Execício #02" />
+            <div className="center">
+                <span className="text">{text}</span>
+                <span className="text">{number}</span>
+                <div>
+                    <button
+                        className="btn"
+                        onClick={() => setNumber(number - 1)}>
+                        -1
+                    </button>
+                    <button
+                        className="btn"
+                        onClick={() => setNumber(number + 1)}>
                         +1
                     </button>
                 </div>
